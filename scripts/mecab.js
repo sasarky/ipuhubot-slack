7f041143572bc83edec13fc0d681146895f01307
sasarky <sasarky@gmail.com>
2014-07-05 18:25:16 +0900

    miss

--- scripts/mecab.js
+++ scripts/mecab.js
@@ -7,7 +7,7 @@ var printf = require('printf');
 
 module.exports = function(robot) {
   robot.hear(/(.*)/i, function(msg) {
-    if (msg.envelope.room == 'Shell') {
+    if (msg.envelope.room == 'ipukun_school') {
       mecab.parse(msg.message.text, function(err, result) {
         result.forEach(function(row) {
           msg.reply(printf("%s (%s)", row[0], row[1]));
