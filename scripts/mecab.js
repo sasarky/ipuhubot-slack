c71441a5904d2786beacfff625134d13dc81a11a
sasarky <sasarky@gmail.com>
2014-07-05 19:12:36 +0900

    いろいろためす

--- scripts/mecab.js
+++ scripts/mecab.js
@@ -9,9 +9,12 @@ module.exports = function(robot) {
   robot.hear(/(.*)/i, function(msg) {
     if (msg.envelope.room == 'ipukun_school') {
       mecab.parse(msg.message.text, function(err, result) {
-        result.forEach(function(row) {
-          msg.reply(printf("%s (%s)", row[0], row[1]));
+        // 単語ごとに区切ったやつであれこれ
+        var sentence = [];
+        result.forEach(function(word) {
+          sentence.push(printf("%s(%s)", word[0], word[1]));
         });
+        msg.reply(sentence);
       });
     }
   });
