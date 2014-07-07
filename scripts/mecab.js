b64efd1b741ff6f402d7e4e4f08300bf70e33b43
sasarky <sasarky@gmail.com>
2014-07-07 23:40:07 +0900

    test

--- scripts/mecab.js
+++ scripts/mecab.js
@@ -3,18 +3,15 @@
 //
 
 var mecab = require('mecab-async');
+var request = require('request');
 var printf = require('printf');
 
 module.exports = function(robot) {
   robot.hear(/(.*)/i, function(msg) {
     if (msg.envelope.room == 'ipukun_school') {
-      mecab.parse(msg.message.text, function(err, result) {
-        // 単語ごとに区切ったやつであれこれ
-        var sentence = [];
-        result.forEach(function(word) {
-          sentence.push(printf("%s(%s)", word[0], word[1]));
-        });
-        msg.reply(sentence);
+      console.log(msg.match[0]);
+      request.get('http://sasarky.net:8888/talk/' + msg.match[0], function(err, res, body) {
+        console.log(body);
       });
     }
   });
