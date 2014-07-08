00ed931ecbdb6580a983d52106655efad38dd200
sugawara ryousuke <rsksound@gmail.com>
2014-07-09 04:32:08 +0900

    微修正

--- src/class/ipuhubot-today.js
+++ src/class/ipuhubot-today.js
@@ -4,26 +4,26 @@
 var parseString = require('xml2js').parseString; //xml perser
 var request = require('request');
 var Today = function(){
-	this.api = 'http://www.mizunotomoaki.com/wikipedia_daytopic/api.cgi/';
+  this.api = 'http://www.mizunotomoaki.com/wikipedia_daytopic/api.cgi/';
 };
 
 Today.prototype.get =  function(arg, callback) {
-	var self = this;
-	request.get(self.api + arg, function(error, response, body){
-		if(error){
-			console.log('connect error');
-			return;
-		}
+  var self = this;
+  request.get(self.api + arg, function(error, response, body){
+    if(error){
+      console.log('connect error');
+      return;
+    }
 
-		parseString(body, function (err, result) {
-			if(err){
-				console.log('perse error');
-				return;
-			}
+    parseString(body, function (err, result) {
+      if(err){
+        console.log('perse error');
+        return;
+	}
 
-			callback(result);
-		});
-	});
+	callback(result);
+    });
+  });
 };
 
-module.exports = new Today;
+module.exports = new Today();
