2848361bb19a758ec22018ae12004c8c4a679486
isseium <issei@ikmz.net>
2014-07-10 20:41:12 +0900

    robot から request に変更

--- scripts/trip.js
+++ scripts/trip.js
@@ -1,22 +1,17 @@
 // Description:
-//   ipukun reccomends popular spot at random from CheekiTrip
+//   ipukun recommends popular spot at random from CheekiTrip
 //
 // Commands:
 //   hubot trip
-github = require('githubot');
+var request = require('request');
 
 module.exports = function(robot) {
   robot.respond(/TRIP$/i, function (msg) {
-    robot.http("http://api.cheekitrip.com/api/rank/spot/popular")
-    .query({
-      limit: 100,
-    })
-    .get()(function(err, res, body) {
+    request.get("http://api.cheekitrip.com/api/rank/spot/popular?limit=100", function(err, res, body){
       var spots = JSON.parse(body);
       var spot  = msg.random(spots);
       
-      robot.http("http://graph.facebook.com/" + spot.spot._id )
-      .get()(function(err, res, body) {
+      request.get("http://graph.facebook.com/" + spot.spot._id, function(err, res, body) {
         var fb_spot = JSON.parse(body);
         var website = (fb_spot.website)?("\n公式Webサイトはこちら " + fb_spot.website):"";
         msg.send("ここ行ってみたら？    " + fb_spot.name + " (" + spot.spot.location.prefecture.name + spot.spot.location.city.name + ") " + fb_spot.link + website);
