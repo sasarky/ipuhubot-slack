ee92bacae6f90e29da7435e923e9f7eb32e78faa
sasarky <sasarky@gmail.com>
2014-09-11 23:24:41 +0900

    部屋全部に対応

--- scripts/imakita.js
+++ scripts/imakita.js
@@ -4,44 +4,41 @@ var printf = require('printf')
 var url = require("url");
 var redis = require('redis');
 var client;
-if (process.env.REDISTOGO_URL) {
-  var rtg = url.parse(process.env.REDISTOGO_URL);
-  client = redis.createClient(rtg.port, rtg.hostname);
-  client.auth(rtg.auth.split(":")[1]);
-} else {
+//if (process.env.REDISTOGO_URL) {
+//  var rtg = url.parse(process.env.REDISTOGO_URL);
+//  client = redis.createClient(rtg.port, rtg.hostname);
+//  client.auth(rtg.auth.split(":")[1]);
+//} else {
   client = redis.createClient();
-}
+//}
 
 module.exports = function(robot) {
-  robot.hear(/.+$/i, function(msg) {
+  // BOT の名前とってきたい
+  robot.hear(/^[^(ipukun)].*$/, function(msg) {
     var user_name = msg.message.user.name;
-    var room_name = msg.message.room;
-    if (room_name == 'ipuhubot') {
-      client.get('hubot:imakita', function(err, val) {
-        val = JSON.parse(val);
-        if (val == null) {
-          val = [];
-        }
-        val.push(msg.message.text);
+    var key = printf('hubot:imakita:%s', msg.message.room);
+    client.get(key, function(err, val) {
+      val = JSON.parse(val);
+      if (val == null) {
+        val = [];
+      }
+      val.push(user_name + ": " + msg.message.text);
 
-        if (val.length > 100) {
-          val.shift();
-        }
-        client.set('hubot:imakita', JSON.stringify(val));
-      });
-    }
+      if (val.length > 100) {
+        val.shift();
+      }
+      client.set(key, JSON.stringify(val));
+    });
   });
 
   robot.respond(/imakita$/i, function(msg) {
-    var room_name = msg.message.room;
-    if (room_name == 'ipuhubot') {
-      client.get('hubot:imakita', function(err, val) {
-        val = JSON.parse(val);
-        var sangyo = _.sample(val, 3);
-        _.each(sangyo, function(v) {
-          msg.send(v);
-        });
+    var key = printf('hubot:imakita:%s', msg.message.room);
+    client.get(key, function(err, val) {
+      val = JSON.parse(val);
+      var sangyo = _.sample(val, 3);
+      _.each(sangyo, function(v) {
+        msg.send(v);
       });
-    }
+    });
   });
 }
