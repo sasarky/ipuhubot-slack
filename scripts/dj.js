2e6b8fb315004043300b71bba3f85a8806eae60f
sasarky <sasarky@gmail.com>
2014-10-06 01:33:42 +0900

    dj

--- scripts/dj.js
+++ scripts/dj.js
@@ -4,10 +4,34 @@
 // Commands:
 //   hubot dj
 github = require('githubot');
+var url = require("url");
+var redis = require('redis');
+var client;
+if (process.env.REDISTOGO_URL) {
+  var rtg = url.parse(process.env.REDISTOGO_URL);
+  client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+  client = redis.createClient();
+}
 
 module.exports = function(robot) {
-  robot.respond(/DJ\sSHOW$/i, function (msg) {
+  robot.respond(/DJ\sADD\s(.*)$/i, function (msg) {
+    var video_id = msg.match[1];
+    client.get('hubot:dj:queue', function(err, val) {
+      var queues = {};
+      if (val) {
+        queues = JSON.parse(val);
+      }
+      queues[video_id] = {
+        id: video_id
+      }
+      console.log(queues);
+      client.set('hubot:dj:queue', JSON.stringify(queues));
+    });
+  });
 
+  robot.respond(/DJ\sSHOW$/i, function (msg) {
     github.get("https://api.github.com/gists/c093283b1b5f9d2df377", function (info) {
       content = info.files.sound_list.content;
       list = content.split("\n");
